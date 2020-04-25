3484630209de78f6c7659100534c4ec15d988133
Романова Ирина Андреевна <irina.an.romanova@rt.ru>
2020-04-25 21:48:47 +0300

    Adds my-routes.js and offers-routes.js

--- src/express/index.js
+++ src/express/index.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const express = require(`express`);
+const myRoutes = require(`./routes/my-routes`);
+const offersRoutes = require(`./routes/offers-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
 const PORT = 8080;
 
 const app = express();
 
+app.use(`/my`, myRoutes);
+app.use(`/offers`, offersRoutes);
 app.use(`/`, mainRoutes);
 app.listen(PORT);
